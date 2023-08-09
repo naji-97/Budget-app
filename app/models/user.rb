41e7abec664297d50673dadd695bd8aba4c6cd8c
Naji-97 <elbairabennaji@gmail.com>
2023-08-09 20:23:53 +0100

    style transaction page and forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable,
-   :validatable
+         :validatable
 
   has_many :transaction_budgets, foreign_key: :author_id, dependent: :destroy
   has_many :categories, foreign_key: :author_id, dependent: :destroy
