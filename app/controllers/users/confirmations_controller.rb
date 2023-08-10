f8eb4f3cf1d9d055d6807c8f1865e78ed1b65d53
Naji-97 <elbairabennaji@gmail.com>
2023-08-10 23:05:47 +0100

    update confiramtion controller

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,3 +1,5 @@
-class Users::ConfirmationsController < Devise::ConfirmationsController
-  # Use the default behavior provided by Devise ConfirmationsController
+module Users
+  class Users::ConfirmationsController < Devise::ConfirmationsController
+    # Use the default behavior provided by Devise ConfirmationsController
+  end
 end
